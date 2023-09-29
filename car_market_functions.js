aff7257a49e80d87a099f661830608c22349568b
osaydayoub <osaydayoub@gmail.com>
2023-09-29 19:52:44 +0300

    Retrieve all agencies added

--- car_market_functions.js
+++ car_market_functions.js
@@ -1,2 +1,27 @@
-const carMarket =  require("./obj.js");
+const carMarket = require("./obj.js");
 //console.log(carMarket.sellers[0]);
+
+// 1. Agency Operations
+// Search for a car agency by its name or ID.
+carMarket.getCarAgency = function (nameOrId) {
+    this.sellers.forEach(agency => {
+        if (agency.agencyName === nameOrId || agency.agencyId === nameOrId) {
+            console.log(agency);
+        }
+    })
+}
+// carMarket.getCarAgency("26_IPfHU1");
+// carMarket.getCarAgency("Best Deal");
+
+// Retrieve all agencies' names.
+carMarket.getAllAgencies = function () {
+    const agenciesNames = [];
+    this.sellers.forEach(agency => {
+        agenciesNames.push(agency.agencyName);
+    })
+    console.log(agenciesNames);
+}
+//test
+//carMarket.getAllAgencies();
+
+
